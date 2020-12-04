f8917d4674b56227e92418d36c5ee148df19644f
princewhyte2 <princewhyte02@gmail.com>
2020-12-04 21:53:07 +0100

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(function() {
+    $("#reserveTable").click(
+        function() {
+            $("#reserveTable").attr("data-toggle", "modal");
+            $("#reserveTable").attr("data-target", "#reservationModal");
+
+        }
+    );
+    $("#login").click(
+        function() {
+            $("#login").attr("data-toggle", "modal");
+            $("#login").attr("data-target", "#loginModal");
+
+        }
+    );
+
+    $("#mycarousel").carousel({
+        interval: 2000
+    });
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        } else {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+});
